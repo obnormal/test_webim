51b8f49c26a1ef56ff0f292134f61f035b8a7350
kamil <kamilgabdullin@gmail.com>
2020-06-27 17:06:19 +0300

    added domain

--- authVK/views.py
+++ authVK/views.py
@@ -1,5 +1,5 @@
 import requests
-from allauth.socialaccount.models import SocialAccount
+from allauth.socialaccount.models import SocialAccount, SocialToken
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
@@ -14,9 +14,10 @@ def auth_page_view(request):
 
 @login_required(login_url='auth_page/')
 def get_friends_view(request):
-    uid = SocialAccount.objects.get(user_id=request.user.id).uid
+    social_account = SocialAccount.objects.get(user_id=request.user.id)
+    access_token = SocialToken.objects.get(account_id=social_account.id).token
     try:
-        response = requests.get('https://api.vk.com/method/friends.get?user_id={}&order=random&count=5&fields=photo_100&access_token=2d05bd122d05bd122d05bd121a2d777f9c22d052d05bd1273f6eb4c98f05580c32cb469&v=5.110'.format(uid))
+        response = requests.get('https://api.vk.com/method/friends.get?user_id={}&order=random&count=5&fields=photo_100&access_token={}&v=5.110'.format(social_account.uid, access_token))
         friends_list = response.json()['response']['items']
         if friends_list:
             context = {'user': request.user,
